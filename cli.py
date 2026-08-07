6e3c9dbf22f346b3915fc1cf313626e8d445a784
unknown <phmacedoalves@gmail.com>
2026-08-07 14:07:51 -0300

    Programando as opções digitadas pelo usuário (0 a 4)

--- cli.py
+++ cli.py
@@ -20,5 +20,59 @@ def main():
         print("0. Sair")
         opcao = input("> ")
 
+        if opcao == "0":
+            break
+
+        elif opcao == "1":
+            for usuario in catalogo.listar_usuarios():
+                print(usuario)
+
+        elif opcao == "2":
+            usuario_digitado = input("Nome do usuário: ")
+            usuario_id = catalogo.buscar_usuario_por_nome(usuario_digitado)
+            playlist = catalogo.playlist_de(usuario_id)
+            if usuario_id is not None:
+                for conteudo in playlist:
+                    print(conteudo)
+            else:
+                print("Usuário não encontrado.")
+
+        elif opcao == "3":
+            usuario_digitado = input("Nome do usuário: ")
+            usuario_id = catalogo.buscar_usuario_por_nome(usuario_digitado)
+            if usuario_id is not None:
+                print(f"Playlist de {usuario_digitado} tem {len(catalogo.playlist_de(usuario_id))} itens. (Posições 1 a {len(catalogo.playlist_de(usuario_id))}). ")
+                posicao_humana = int(input("Posição: "))
+                conteudo = catalogo.conteudo_na_posicao(usuario_id, posicao_humana - 1)
+                if conteudo is not None:
+                    print(f"Posição {posicao_humana} de {usuario_digitado}: {conteudo}")
+                else:
+                    print("Posição inválida.")
+            else:
+                print("Usuário não encontrado.")
+
+        elif opcao == "4":
+            usuarios_digitados = input("Nomes dos usuários separados por vírgula (ex.: Nicholas, Uchoa): ")
+            usuario_ids = []
+            if len(usuarios_digitados.split(",")) == 1:
+                print("Informe pelo menos dois usuários.")
+            for nome in usuarios_digitados.split(","):
+                usuario_id = catalogo.buscar_usuario_por_nome(nome.strip())
+                if usuario_id is None:
+                    print("Sem interseção.")
+                    break
+                usuario_ids.append(usuario_id)
+            else:
+                intersecao = catalogo.intersecao_playlists(usuario_ids)
+                if intersecao:
+                    print(f"Interseção: {len(intersecao)} conteúdos:")
+                    for conteudo in intersecao:
+                        print(conteudo)
+                else:
+                    print("Sem interseção.")
+
+        elif opcao == "5":
+
+            
 if __name__ == "__main__":
     main()
